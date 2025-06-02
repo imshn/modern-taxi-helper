2ec1a196772b068187dfc28836eceb58c73a5bca
iamsaziya <saziyaaansari.30@gmail.com>
2025-06-02 14:33:25 +0530

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -117,14 +117,14 @@ const Services = () => {
           </Link>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
             <ServiceCard 
               key={service.id} 
               service={service}
             />
           ))}
-        </div>
+        </div> */}
       </div>
     </section>
   );
